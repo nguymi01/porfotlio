4437fdabb409c03a98917e84edb62b46c9dd0926
minh231299 nguyen <Minhmovies99@gmail.com>
2024-09-13 07:04:56 +0700

    done

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,30 @@
+import { Container } from './styles';
+import emailIcon from '../../assets/email-icon.svg';
+import phoneIcon from '../../assets/phone-icon.svg';
+import { Form } from '../Form/Form';
+
+export function Contact() {
+  return (
+    <Container id='contact'>
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for a Free consultation.</p>
+      </header>
+      <div className='contacts'>
+        <div>
+          <a href='mailto:minhdnguyen99@gmail.com'>
+            <img src={emailIcon} alt='Email' />
+          </a>
+          <a href='mailto:minhdnguyen99@gmail.com'>minhdnguyen99@gmail.com</a>
+        </div>
+        <div>
+          <a href='tel:+17174200855'>
+            <img src={phoneIcon} alt='Phone No' />
+          </a>
+          <a href='tel:+17174200855'>(+1) 7174200855</a>
+        </div>
+      </div>
+    </Container>
+  );
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/minh-bio/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/nguymi01"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+      </div>
+    </Container>
+  )
+}
